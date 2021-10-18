94dfd4da3818a2e0e5a96d51d1300bb004b2cadb
bbalakriz <balakrishnan.b@gmail.com>
2021-10-18 18:22:34 +0800

    first

---
+++ src/main/java/com/bala/messaging/JMSSignalReceiver.java
@@ -0,0 +1,258 @@
+package com.bala.messaging;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.ServiceLoader;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import org.apache.commons.io.input.ClassLoaderObjectInputStream;
+import org.jbpm.process.instance.impl.ProcessInstanceImpl;
+import org.jbpm.process.instance.impl.util.VariableUtil;
+import org.jbpm.workflow.instance.node.EventNodeInstance;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.process.NodeInstance;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkflowProcessInstance;
+import org.kie.internal.runtime.manager.InternalRuntimeManager;
+import org.kie.internal.runtime.manager.RuntimeManagerIdFilter;
+import org.kie.internal.runtime.manager.RuntimeManagerRegistry;
+import org.kie.internal.runtime.manager.context.ProcessInstanceIdContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JMSSignalReceiver
+ */
+@org.jboss.ejb3.annotation.ResourceAdapter(value = "activemq-rar.rar") // ONLY FOR REMOTE AMQ
+public class JMSSignalReceiver implements MessageListener {
+    private static final Logger logger = LoggerFactory.getLogger(JMSSignalReceiver.class);
+
+    private static final ServiceLoader<RuntimeManagerIdFilter> runtimeManagerIdFilters = ServiceLoader
+            .load(RuntimeManagerIdFilter.class);
+
+    @Override
+    public void onMessage(Message message) {
+        if (message instanceof BytesMessage) {
+
+            String deploymentId;
+            Long processInstanceId;
+            String signal;
+            Object data;
+
+            BytesMessage bytesMessage = (BytesMessage) message;
+            RuntimeManager runtimeManager = null;
+            RuntimeEngine engine = null;
+
+            int retries = Integer.getInteger("com.sample.signal.retries", 30);
+            int delay = Integer.getInteger("com.sample.signal.retry.interval", 5000);
+
+            try {
+                deploymentId = (String) bytesMessage.getObjectProperty("DeploymentId");
+                signal = (String) bytesMessage.getObjectProperty("Signal");
+                processInstanceId = (Long) bytesMessage.getObjectProperty("ProcessInstanceId");
+
+                logger.debug("Deployment id '{}', Signal '{}', ProcessInstanceId '{}'", deploymentId, signal,
+                        processInstanceId);
+
+                Collection<String> availableRuntimeManagers = matchDeployments(deploymentId,
+                        RuntimeManagerRegistry.get().getRegisteredIdentifiers());
+
+                for (String matchedDeploymentId : availableRuntimeManagers) {
+                    try {
+                        runtimeManager = RuntimeManagerRegistry.get().getManager(matchedDeploymentId);
+
+                        if (runtimeManager == null) {
+                            throw new IllegalStateException(
+                                    "There is no runtime manager for deployment " + matchedDeploymentId);
+                        }
+                        logger.debug(
+                                "RuntimeManager found for deployment id {}, reading message content with custom class loader of the deployment",
+                                matchedDeploymentId);
+                        data = readData(bytesMessage,
+                                ((InternalRuntimeManager) runtimeManager).getEnvironment().getClassLoader());
+                        logger.debug("Data read successfully with output {}", data);
+
+                        engine = runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get(processInstanceId));
+                        System.out.println("=========================================");
+                        System.out.println("Signal is:" + signal);
+                        System.out.println("=========================================");
+
+                        // perform operation for signal
+                        if (signal != null) {
+                            if (processInstanceId != null) {
+                                logger.debug(
+                                        "About to signal process instance with id {} and event data {} with signal {}",
+                                        processInstanceId, data, signal);
+
+                                int attempt = 0;
+                                while (true) {
+                                    if (attempt > retries) {
+                                        // TODO: If the retries exceed the set limit, push the message to a DLQ
+                                        break;
+                                    }
+                                    // get process instance in readonly mode to avoid locks
+                                    ProcessInstance processInstance = engine.getKieSession()
+                                            .getProcessInstance(processInstanceId, true);
+
+                                    // use runtime snapshot and get the active nodes
+                                    if (processInstance != null) {
+                                        ((ProcessInstanceImpl) processInstance).setProcess(engine.getKieSession()
+                                                .getKieBase().getProcess(processInstance.getProcessId()));
+                                        Collection<NodeInstance> activeNodes = ((WorkflowProcessInstance) processInstance)
+                                                .getNodeInstances();
+
+                                        // get only active signal nodes
+
+                                        Collection<String> activeNodesComposite = new ArrayList<>();
+                                        System.out.println("=================================");
+                                        System.out.println("All active signals in the process are: "
+                                                + getActiveSignalNodes(activeNodes, activeNodesComposite));
+
+                                        // if the incoming signal is in active state, fire it
+                                        if (activeNodesComposite.contains(signal)) {
+                                            System.out.println("=============== Firing Signal for " + processInstanceId
+                                                    + " ==================");
+                                            engine.getKieSession().signalEvent(signal, data, processInstanceId);
+                                            break;
+                                        } else { // wait and retry
+                                            attempt++;
+                                            try {
+                                                Thread.sleep(delay);
+                                            } catch (InterruptedException e1) {
+                                                logger.trace("retry sleeping got interrupted");
+                                            }
+                                        }
+                                    }
+                                }
+
+                            } else {
+                                logger.debug("About to broadcast signal {} and event data {}", signal, data);
+                                runtimeManager.signalEvent(signal, data);
+                            }
+                            logger.debug("Signal completed successfully for signal {} with data {}", signal, data);
+                        } else {
+                            logger.warn("No signal or workitem id is given, skipping this message");
+                        }
+                    } catch (Exception e) {
+                        logger.error("Unexpected exception while signaling: {}", e.getMessage(), e);
+                    } finally {
+                        if (runtimeManager != null && engine != null) {
+                            runtimeManager.disposeRuntimeEngine(engine);
+                        }
+                    }
+                }
+            } catch (
+
+            Exception e) {
+                logger.error("Unexpected exception while processing signal JMS message: {}", e.getMessage(), e);
+            }
+        }
+
+    }
+
+    // collect only active signal nodes
+    Collection<String> getActiveSignalNodes(Collection<NodeInstance> activeNodes,
+            Collection<String> activeNodesComposite) {
+        for (NodeInstance nodeInstance : activeNodes) {
+            if (nodeInstance instanceof EventNodeInstance) {
+                String type = ((EventNodeInstance) nodeInstance).getEventNode().getType();
+                if (type != null && !type.startsWith("Message-")) {
+                    activeNodesComposite.add(VariableUtil.resolveVariable(type, nodeInstance));
+                }
+            }
+        }
+
+        return activeNodesComposite;
+    }
+
+    protected Object readData(BytesMessage message, ClassLoader cl) throws JMSException, Exception {
+        Object data = null;
+        if (message.getBodyLength() > 0) {
+            byte[] reqData = new byte[(int) message.getBodyLength()];
+
+            message.readBytes(reqData);
+            if (reqData != null) {
+                ObjectInputStream in = null;
+                try {
+                    in = new ClassLoaderObjectInputStream(cl, new ByteArrayInputStream(reqData));
+                    data = in.readObject();
+                } catch (IOException e) {
+                    logger.warn("Exception while serializing context data", e);
+
+                } finally {
+                    if (in != null) {
+                        in.close();
+                    }
+                }
+            }
+        }
+        return data;
+    }
+
+    protected Collection<String> matchDeployments(String deploymentId, Collection<String> availableDeployments) {
+        if (availableDeployments == null || availableDeployments.isEmpty()) {
+            return Collections.emptyList();
+        }
+        Collection<String> matched = new ArrayList<String>();
+        for (RuntimeManagerIdFilter filter : runtimeManagerIdFilters) {
+            matched = filter.filter(deploymentId, availableDeployments);
+
+            if (matched != null && !matched.isEmpty()) {
+                return matched;
+            }
+        }
+
+        // nothing matched return given deployment id
+        return Collections.singletonList(deploymentId);
+    }
+
+    void oldTrial() {
+
+        // List<NodeInstanceLog> log = engine.getAuditService()
+        // .findActiveProcessInstances(processInstanceId).;
+
+        // NodeInstanceContainer processInstance = (WorkflowProcessInstance)
+        // engine.getKieSession()
+        // .getProcessInstance(processInstanceId);
+        // Collection<NodeInstance> activeInstances =
+        // processInstance.getNodeInstances();
+
+        // System.out.println("===========================================");
+        // System.out.println("Process instance id:" + processInstanceId);
+
+        // System.out.println("Process id:" + processInstance.);
+        // System.out.println("Has Signal Completion:" +
+        // processInstance.hasSignalCompletion());
+        // System.out.println("Has Signal Completion:" + processInstance.);
+        // System.out.println("=========================================");
+
+        // for (NodeInstance nodeInstance : activeNodes) {
+        // if (nodeInstance instanceof EventNodeInstance) {
+        // String type = ((EventNodeInstance) nodeInstance).getEventNode().getType();
+
+        // for (NodeInstance nodeInstance : activeInstances) {
+        // System.out.println("=========================================");
+        // // System.out.println("Node name is:" + nodeInstance.getNodeName());
+        // // System.out.println("Node Id is:" + nodeInstance.getNodeId());
+        // System.out.println("=========================================");
+        // if (nodeInstance instanceof EventNodeInstance) {
+        // String uniqueId = ((EventNodeInstance) nodeInstance).getEventNode()
+        // .getUniqueId();
+        // String type = ((EventNodeInstance) nodeInstance).getEventNode().getType();
+        // System.out.println("============================================");
+        // System.out.println("Node type and uniqueid is:" + type + "," + uniqueId);
+
+        // engine.getKieSession().signalEvent(signal, data, processInstanceId);
+        // }
+        // }
+    }
+}
